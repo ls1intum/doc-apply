bc6057982289b39b1c91b592984e7d3d4deb548a
Melissa <ge94zec@tum.de>
2026-08-21 22:03:30 +0200

    - add JavaDoc to GenderCategory

--- src/main/java/de/tum/cit/aet/core/constants/GenderCategory.java
+++ src/main/java/de/tum/cit/aet/core/constants/GenderCategory.java
@@ -1,5 +1,10 @@
 package de.tum.cit.aet.core.constants;
 
+/**
+ * Identifies which {@link GenderBiasWordLists} category matched a word during analysis:
+ * NON_INCLUSIVE findings lower the gender score, while INCLUSIVE findings
+ * contribute as a counterweight when both categories occur.
+ */
 public enum GenderCategory {
     NON_INCLUSIVE,
     INCLUSIVE,
